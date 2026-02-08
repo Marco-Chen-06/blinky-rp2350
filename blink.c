6a3a1ab8821b35af5517cbdd65b844ee1a7bf9b2
Marco-Chen-06 <marcochen.work@gmail.com>
2026-02-07 20:41:35 -0500

    removed unnecessary headers

--- blink.c
+++ blink.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "pico/binary_info.h"
-#include "hardware/structs/iobank0.h"
 #include "hardware/structs/sio.h"
+#include "hardware/structs/iobank0.h"
 #include "hardware/regs/io_bank0.h"
 #include "hardware/structs/padsbank0.h"
-#include "hardware/structs/uart.h"
 const uint LED_PIN = 25;
 
 int main() {
-    uart_inst_t awawd;
-
-
-    bi_decl(bi_program_description("BLINK TEST BINARY"));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
-    
     // because i missed one line on the datasheet so i got stuck debugging a blinking led forever
     hw_clear_bits(&pads_bank0_hw->io[LED_PIN], PADS_BANK0_GPIO0_OD_BITS);
 
